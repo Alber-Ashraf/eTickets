6e90f49de1d43240c7d78ce1d5d8a2e2bf5983e7
Bero Ashraf <beroooberooo6@gmail.com>
2022-07-05 12:07:19 +0200

    Add Controllers

---
+++ eTickets/Controllers/CinemasContoller.cs
@@ -0,0 +1,22 @@
+﻿using eTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+    public class CinemasContoller : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasContoller(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Cenimas.ToListAsync();
+            return View();
+        }
+    }
+}
