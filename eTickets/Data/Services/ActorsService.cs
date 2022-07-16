c39d1f99dd3c1a49ac554ed8b9b58ffc2eba6d32
Bero Ashraf <beroooberooo6@gmail.com>
2022-07-16 17:10:23 +0200

    View-Actor Details

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -14,10 +14,10 @@ namespace eTickets.Data.Services
             _context = cotext;
         }
 
-        public void Add(Actor actor)
+        public async Task AddAsync(Actor actor)
         {
-            _context.Actors.Add(actor);
-            _context.SaveChanges();
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
         }
 
         public void Delete(int id)
@@ -31,14 +31,23 @@ namespace eTickets.Data.Services
             return results;
         }
 
-        public Actor GetById(int id)
+        public async Task<IEnumerable<Actor>> GetAllAsync()
         {
-            throw new System.NotImplementedException();
+            var result = await _context.Actors.ToListAsync();
+            return result;
         }
 
-        public Actor Update(int id, Actor newActor)
+        public async Task<Actor> GetByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var results = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            return results;
+        }
+
+        public async Task<Actor> UpdateAsync(int id, Actor newActor)
+        {
+            _context.Update(newActor);
+            await _context.SaveChangesAsync();
+            return newActor;
         }
     }
 }
