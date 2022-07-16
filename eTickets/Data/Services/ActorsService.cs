a81a3b075a3897530003c669ea3c89345a8a22d9
Bero Ashraf <beroooberooo6@gmail.com>
2022-07-16 17:31:47 +0200

    Delete-Actor

--- eTickets/Data/Services/ActorsService.cs
+++ eTickets/Data/Services/ActorsService.cs
@@ -20,11 +20,12 @@ namespace eTickets.Data.Services
             await _context.SaveChangesAsync();
         }
 
-        public void Delete(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new System.NotImplementedException();
+            var results = await _context.Actors.FirstOrDefaultAsync(n => n.Id == id);
+            _context.Actors.Remove(results);
+            await _context.SaveChangesAsync();
         }
-
         public async Task<IEnumerable<Actor>> GetAll()
         {
             var results = await _context.Actors.ToListAsync();
