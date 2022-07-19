fbb161cd1d0e29c704ec0079c9466732ced778ff
Bero Ashraf <beroooberooo6@gmail.com>
2022-07-19 14:18:50 +0200

    Add-Order View

---
+++ eTickets/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿using eTickets.Data.Cart;
+using eTickets.Data.Services;
+using eTickets.Data.ViewModels;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IMoviesService _moviesservice;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrdersController(IMoviesService moviesservice, ShoppingCart shoppingCart)
+        {
+            _moviesservice = moviesservice;
+            _shoppingCart = shoppingCart;
+        }
+
+        public IActionResult Index()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var response = new ShoppingCartVM()
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(response);
+        }
+    }
+}
--- eTickets/Data/Cart/ShoppingCart.cs
+++ eTickets/Data/Cart/ShoppingCart.cs
@@ -18,6 +18,48 @@ namespace eTickets.Data.Cart
             _context = context;
         }
 
+        //Add Item to Cart
+        public void AddItemToCart(Movie movie)
+        {
+            var ShoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if(ShoppingCartItem == null)
+            {
+                ShoppingCartItem = new ShoppingCartItem()
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Movie = movie,
+                    Amount = 1
+                };
+                _context.ShoppingCartItems.Add(ShoppingCartItem);
+            }
+            else
+            {
+                ShoppingCartItem.Amount++;
+            }
+            _context.SaveChanges();
+        }
+
+        //Remove Item from cart
+        public void RemovetemFromCart(Movie movie)
+        {
+            var ShoppingCartItem = _context.ShoppingCartItems.FirstOrDefault(n => n.Movie.Id == movie.Id && n.ShoppingCartId == ShoppingCartId);
+
+            if (ShoppingCartItem != null)
+            {
+                if(ShoppingCartItem.Amount > 1)
+                {
+                    ShoppingCartItem.Amount--;
+                }
+                else
+                {
+                    _context.ShoppingCartItems.Remove(ShoppingCartItem);
+                }
+            }
+            _context.SaveChanges();
+        }
+        
+        //Get all shopping cart items
         public List<ShoppingCartItem> GetShoppingCartItems()
         {
             return ShoppingCartItems ?? (ShoppingCartItems = _context.ShoppingCartItems.Where(n => n.ShoppingCartId ==
