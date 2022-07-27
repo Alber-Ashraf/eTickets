52e7c8639867e42e60a178666610f43d254593d6
Bero Ashraf <beroooberooo6@gmail.com>
2022-07-27 18:05:15 +0200

    Add-Sign up user

--- eTickets/Data/ViewModels/RegisterVM .cs
+++ eTickets/Data/ViewModels/RegisterVM .cs
@@ -4,11 +4,11 @@ namespace eTickets.Data.ViewModels
 {
     public class RegisterVM
     {
-        [Display(Name = "Full Name")]
+        [Display(Name = "Full name")]
         [Required(ErrorMessage = "Full name is required")]
         public string FullName { get; set; }
 
-        [Display(Name = "Email Address")]
+        [Display(Name = "Email address")]
         [Required(ErrorMessage = "Email address is required")]
         public string EmailAddress { get; set; }
 
